ba7128711c9e5462fc44a027ca99507958c09020
Julio <juliocaue22@gmail.com>
2026-06-21 20:25:07 -0300

    falar_mic adicionado novamente. Agora sem mensagem de erro.

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from ai import llm as IA
 from translators import text_to_speech as TTS
 from translators import speech_to_text as STT
 from animation import falar_audio as dublar
-
-#live mouth removido por enquanto para evitar erros
+from animation import falar_mic
+import subprocess
+import os
 
 while True:
+    subprocess.run('cls' if os.name == 'nt' else 'clear')
     escolha=input("Oque deseja usar?\n\n1- Boca ao vivo\n2- Perguntar IA\n\nEscolha (1/2): ")
     if escolha == "1":
-        try:
-            pass #removido por enquanto para lidar com mensagens de erro depois
-        except KeyboardInterrupt:
-            continue
+        falar_mic.imitar_fala()
+        continue
     else:
         try:
             TTS.voz_para_wav(IA.perguntar_ia(STT.pegar_transcricao()))
