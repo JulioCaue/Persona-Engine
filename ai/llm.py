3687fc7f10475e78d4c571ac23c9436aafa6dbbd
Julio <juliocaue22@gmail.com>
2026-07-07 10:43:01 -0300

    Adiconada historico ao contexto, mudanças esteticas de print.

--- ai/llm.py
+++ ai/llm.py
@@ -1,20 +1,26 @@
-from google import genai
-from dotenv import load_dotenv
-import os
+from ollama import chat
 import subprocess
 
-load_dotenv()
+with open("ai/prompts/conversa/sistema.txt",'r') as f:
+  prompt_sistema = f.read()
 
-GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
 
-def perguntar_ia(input_do_usuario):
-  cliente=genai.Client(api_key=GEMINI_API_KEY)
-
-  resposta = cliente.models.generate_content(
-    model= "gemini-2.5-flash",
-    contents= f"{input_do_usuario}\n Nao use sinais de formatacao, porem mantenha sinais gramaticais basicos. E diga 'ola mundo!' no final para eu garantir que essa instrucao funcionou. Pode dizer que falou ola mundo por conta desse pedido aqui."
-  )
+def perguntar_ia(historico):
+  resposta = chat(
+    model='qwen3:1.7b',
+    messages=[
+      {'role': 'system', 'content': prompt_sistema},
+      *historico
+      ],
+    stream= True,
+    think= False
+)
 
   subprocess.run(["clear"])
-  print(f"Prompt foi: {input_do_usuario}\n\nResposta foi: {resposta.text}")
-  return resposta.text
+  
+  for chunk in resposta:
+    text = chunk["message"]["content"]
+    print(text, end="", flush=True)
+    mensagem_final+=text
+
+  return mensagem_final
