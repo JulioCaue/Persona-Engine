9f44fad2e6cf447a5816354234abad71a73cfa9c
Julio <juliocaue22@gmail.com>
2026-07-07 23:38:29 -0300

    Nomes de funções melhorados. Codigo completamente funcional para adiconar tanto texto da ia quanto do usuario.

--- ai/history.py
+++ ai/history.py
@@ -1,19 +1,25 @@
 import json
+import os
 
 local_arquivo = "ai/prompts/conversa/historico.json"
 
-def add_input_to_history(input_do_usuario):
-    with open(local_arquivo,"r") as file:
+def add_message_to_history(input,origem):
+    if not os.path.exists(local_arquivo):
+        with open(local_arquivo,"w",encoding="utf-8") as file:
+            json.dump([], file, ensure_ascii=False, indent=4)
+
+    with open(local_arquivo,"r",encoding="utf-8") as file:
         data = json.load(file)
-        input = {'role': 'user', 'content': input_do_usuario}
+
+    input = {'role': origem, 'content': input}
 
     data.append(input)
     
-    with open(local_arquivo,"w") as file:
-        json.dump(data, file, indent=4)
+    with open(local_arquivo,"w",encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
 def pull_history():
-    with open(local_arquivo,"r") as file:
+    with open(local_arquivo,"r",encoding="utf-8") as file:
         if file is None:
             return None
         else:
