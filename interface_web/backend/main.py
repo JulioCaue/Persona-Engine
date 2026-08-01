11bf703009dcf5bfc39e87cdaeb85172791b2026
Julio <juliocaue22@gmail.com>
2026-08-01 00:24:03 -0300

    API agora funciona de forma assincrona, e apenas o modo mais recente selecionado funciona. O antigo termina em triggers certos e o novo espera ele terminar para poder começar.

--- interface_web/backend/main.py
+++ interface_web/backend/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException
+from serial import SerialException
+import asyncio
+import threading
+import controlador
+from logs import log_writer
+
 
 app = FastAPI()
+ultimo_modo: int
+tarefa_atual: asyncio.Task | None = None
+evento_atual: threading.Event | None = None
+lock_modo = asyncio.Lock()
+
 class ModoEscolha(BaseModel):
     modo: int
 
@@ -14,16 +26,48 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
 
 @app.post("/controle")
-def receber_modo(dados: ModoEscolha):
+async def receber_modo(dados: ModoEscolha):
+    global evento_atual, tarefa_atual, ultimo_modo
     modo_recebido = int(dados.modo)
+    ultimo_modo = modo_recebido
 
-    print(modo_recebido)
+    async with lock_modo:
+        tarefa_anterior = tarefa_atual
+        evento_anterior = evento_atual
+
+        if evento_anterior is not None:
+            evento_anterior.set()
+
+        if tarefa_anterior is not None:
+            try:
+                await tarefa_anterior
+
+            except SerialException:
+                raise HTTPException(
+                    status_code=503,
+                    detail="Arduino não conectado."
+                )
+
+            except Exception as e:
+                print(f"Ocorreu um erro: {e}")
+                log_writer.write(f"Ocorreu um erro: {e}")
+                raise HTTPException(
+                    status_code=500,
+                    detail="Erro interno inesperado"
+                )
+
+        evento_atual = threading.Event()
+
+        tarefa_atual = asyncio.create_task(
+            asyncio.to_thread(
+                controlador.trocar_modo,
+                ultimo_modo,
+                evento_atual
+            )
+        )
+
 
     return {
         "modo_recebido": modo_recebido
