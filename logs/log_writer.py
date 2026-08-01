2722cfb7a872b408c12b65b9a7cb3a2440eb49bb
Julio <juliocaue22@gmail.com>
2026-08-01 00:13:30 -0300

    Adicionada função que lê o log de erros do dia. Função de escrita de erros agora usa essa nova função de leitura para evitar spam.

--- logs/log_writer.py
+++ logs/log_writer.py
@@ -1,17 +1,34 @@
 from datetime import datetime
+import os
+
+caminho_pastas = "logs/errors/"
+ftype = ".txt"
+agora = datetime.now()
+dia_hoje = agora.strftime("%d-%m-%Y")
+hora_agora = agora.strftime("%H:%M")
+
+caminho_arquivo = f"{caminho_pastas}{dia_hoje}{ftype}"
+
+def ler_erros():
+    """
+    Lê todos os erros no arquivo de erros do dia.
+    """
+    with open(caminho_arquivo,"r") as file:
+        return file.read()
 
 def write(error_line):
     """
     Salva erros para um arquivo de texto.
     Arquivos de texto diferentes para cada dia.
     Salva no fim de arquivo caso existente.
+    Precisa apenas da linha de erro.
     """
-    caminho_pastas = "logs/errors/"
-    ftype = ".txt"
-    agora = datetime.now()
-    dia_hoje = agora.strftime("%d-%m-%Y")
-    hora_agora = agora.strftime("%H:%M:%S")
 
-    with open(f"{caminho_pastas}{dia_hoje}{ftype}","a") as file:
-        if error_line not in file:
-            file.write(f"[{hora_agora}]: {error_line}\n")
+    if os.path.exists(caminho_arquivo):
+        erros_no_arquivo = ler_erros()
+    else: erros_no_arquivo = "\n"
+
+    #Escreve um erro de cada tipo apenas a cada um minuto. Remove spam mas ainda facilita visualição de erros para debug.
+    with open(caminho_arquivo,"a") as file:
+        if f"[{hora_agora}]: {error_line}" not in erros_no_arquivo:
+            file.write(f"[{hora_agora}]: {error_line}\n\n")
