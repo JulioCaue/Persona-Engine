7a9b09657dd963a792ca224409c700792e6f428c
Julio <juliocaue22@gmail.com>
2026-08-01 14:49:44 -0300

    Adicionado cheque de existencia do arquivo wav, e caminho trocado para uma variavel.

--- audios/audio_player.py
+++ audios/audio_player.py
@@ -1,12 +1,14 @@
 import scipy.io.wavfile as wav
 import sounddevice as sd
+import os
 from logs import log_writer
-
 
 def Tocar_Wav():
     "Toca um arquivo wav"
-    try:
-        sample_rate,data = wav.read("audios/audio_output.wav")
-        sd.play(data,sample_rate)
-    except Exception as e:
-        log_writer.write(f"Ocorreu um erro ao tocar wav: {e}")
+    local_arquivo_output = "audios/audio_output.wav"
+    if os.path.exists(local_arquivo_output):
+        try:
+            sample_rate,data = wav.read(local_arquivo_output)
+            sd.play(data,sample_rate)
+        except Exception as e:
+            log_writer.write(f"Ocorreu um erro ao tocar wav: {e}")
