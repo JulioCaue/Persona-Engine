308dc8add0e38adf72da677250791943c2c58634
Julio <juliocaue22@gmail.com>
2026-08-17 10:30:51 -0300

    Adicionada verificação de arduino e envio do input.

--- interface_web/frontend/script.js
+++ interface_web/frontend/script.js
@@ -1,14 +1,55 @@
 const listaModos = document.getElementById("modoComportamento")
 const caixa_mensagem = document.getElementById("section_input_mensagem")
-const botao_parar = document.getElementById("botao_parar")
+const btn_parar = document.getElementById("botao_parar")
 const modo_audio = document.getElementById("modo_audio")
+const caixa_input = document.getElementById("campo_input")
+const btn_enviar = document.getElementById("btnEnviar")
+const sinal_arduino = document.getElementById("status_arduino")
 url = "http://127.0.0.1:8000";
 
 
+async function verificar_arduino(){
+    const resposta = await fetch(`${url}/status/arduino`);
+    const status = await resposta.json();
+    if (status){
+        sinal_arduino.classList.add("ativo");
+        sinal_arduino.classList.remove("desativado")
+    }
+    else{
+        sinal_arduino.classList.add("desativado");
+        sinal_arduino.classList.remove("ativo")
+    }
+    return status
+}
+
+async function receberInput(click) {
+    const textoEscrito = caixa_input.value;
+
+    try{
+        const resposta = await fetch(`${url}/controle`,{
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                modo: 3,
+                input: textoEscrito
+            })
+        });
+        
+        const resultado = await resposta.json();
+
+        console.log(resultado)
+    } 
+    catch(erro){
+        console.error("Erro enviando data: ",erro);
+    }
+}
+
 async function trocarAudio(event) {
     const modoEscolhido = event.target.value;
-    if ([1,2].includes(Number(modoEscolhido))){
-        if (Number(modoEscolhido) == 1){escolha = true;}
+    if ([1,2].includes(parseInt(modoEscolhido))){
+        if (parseInt(modoEscolhido) == 1){escolha = true;}
         else{escolha = false;}
         try{
             const resposta = await fetch(`${url}/controle`,{
@@ -17,7 +58,8 @@ async function trocarAudio(event) {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                    modo: escolha
+                    modo: escolha,
+                    input: null
                 })
             });
             
@@ -41,7 +83,8 @@ async function pararModo(onclick) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                modo: modoEscolhido
+                modo: modoEscolhido,
+                input: null
             })
         });
         
@@ -77,7 +120,8 @@ async function trocarModo(event) {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                    modo: modoEscolhido
+                    modo: modoEscolhido,
+                    input: null
                 })
             });
             
@@ -91,8 +135,14 @@ async function trocarModo(event) {
     }
 }
 
+verificar_arduino().then(status => {
+    console.log(status);
+})
+setInterval(verificar_arduino,5000)
 
-botao_parar.addEventListener("click",pararModo);
+btn_enviar.addEventListener("click",receberInput)
+
+btn_parar.addEventListener("click",pararModo);
 
 modo_audio.addEventListener("change",trocarAudio);
 
