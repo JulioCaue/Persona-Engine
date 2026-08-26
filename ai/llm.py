118657fcfbf21dfeb44d40580af23a1ed39d26c4
Julio <juliocaue22@gmail.com>
2026-08-26 14:57:53 -0300

    Grande overhaul. Chamada para IA agora é função separada. Adicionadas ferramentas.

--- ai/llm.py
+++ ai/llm.py
@@ -6,51 +6,126 @@ Pode configurar os parametros da comunicação.
 from ollama import chat
 from ai import history
 from logs import log_writer
+from ai import ferramentas
+from ai.prompts.sistema import system_prompts
 import os
 import threading
 
+musica_atual: str
+
+musicas = [
+    """NOMES DE MUSICAS AQUI!!!"""
+]
+
+caminho_prompt_sistema = "ai/prompts/sistema/system_prompts.py"
+
+if os.path.exists(caminho_prompt_sistema):
+    with open(caminho_prompt_sistema,'r') as f:
+        prompt_sistema = system_prompts.system_prompt()
+
+else: 
+    prompt_sistema = ""
+
+
+tocar_musica_tool = {
+    "type": "function",
+    "function": {
+        "name": "tocar_musica",
+        "description": """
+        Toca uma música.
+        Interprete o que o usuário quis dizer e escolha a música
+        disponível que mais corresponde ao pedido, mesmo que ele
+        escreva o nome de forma diferente ou com pequenos erros.
+        """,
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "nome": {
+                    "type": "string",
+                    "enum": musicas
+                }
+            },
+            "required": ["nome"]
+        }
+    }
+}
+
+parar_musica_tool = {
+    "type": "function",
+    "function": {
+        "name": "parar_musica",
+        "description": """
+        Pare uma música.
+        Interprete o que o usuário quis dizer e escolha a ultima música
+        que o usuario pediu.
+        """,
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "nome": {
+                    "type": "string",
+                    "enum": musicas
+                }
+            },
+            "required": ["nome"]
+        }
+    }
+}
+
+def chamada_ia(prompt_sistema,historico):
+    return chat(
+    model='qwen2.5:3b',
+    messages=[
+        {'role': 'system', 'content': prompt_sistema},
+        {'role': 'system', 'content': system_prompts.test_prompt()},
+        *historico,
+        ],
+    think= False,
+    tools=[tocar_musica_tool,parar_musica_tool],
+    keep_alive="5m"
+    )
 
 def perguntar_ia(
     historico,
     flag_parar: threading.Event):
 
 
-  """Envia prompt para IA."""
+    """Envia prompt para IA."""
 
-  caminho_arquivo = "ai/prompts/sistema.txt"
+    if not prompt_sistema:
+        erro = "Arquivo de prompt do sistema não foi encontrado."
+        log_writer.write(__name__,erro)
+        print(erro)
+        return
+    
+    resposta = chamada_ia(prompt_sistema,historico)
 
-  if os.path.exists(caminho_arquivo):
-    with open(caminho_arquivo,'r') as f:
-      prompt_sistema = f.read()
-  
-  else: 
-    prompt_sistema = ""
-    log_writer.write(__name__,"Arquivo de prompt do sistema não foi encontrado.")
+    if resposta.message.tool_calls:
+        for chamada in resposta.message.tool_calls:
+            try:
+                print("Ferramenta escolhida:", chamada.function.name)
+                print("Argumentos:", chamada.function.arguments)
+                if chamada.function.name:
+                    nome_funcao = chamada.function.name
+                    funcao = getattr(ferramentas, nome_funcao)
+                    argumentos = chamada.function.arguments
+                    resultado = funcao(**argumentos)
+                    print("Resultado:", resultado)
+            except Exception as e:
+                mensagem_de_erro = (
+                    f"Erro na ferramenta '{chamada.function.name}' com argumentos {chamada.function.arguments}: {e}\n"
+                )
+            input = {'role': 'tool', 'content': mensagem_de_erro}
+            historico.append(input)
+            resposta = chamada_ia(prompt_sistema,historico)
+            history.add_message_to_history(mensagem_de_erro, "tool")
+            print ("passou de resposta no erro")
 
-  mensagem_final = ""
+    print (f"resposta atual:  {resposta.message.content}\n\n")
+    if not flag_parar.is_set() and resposta.message.content:
+        mensagem_final = resposta.message.content
+        print(f"\n\nchegou em mensagem final: {mensagem_final}\n\n")
+        #Salva mensagem da IA no historico também.
+        history.add_message_to_history(mensagem_final,"assistant")
 
-  resposta = chat(
-    model='qwen3:1.7b',
-    messages=[
-      {'role': 'system', 'content': prompt_sistema},
-      *historico
-      ],
-    stream= True,
-    think= None
-)
-  
-  #Serve para que cada letra apareça no momento que for gerada,
-  #ao envés de tudo quando a mensagem final estiver pronta
-  for chunk in resposta:
-    text = chunk["message"]["content"]
-    print(text, end="", flush=True)
-    mensagem_final += text
-    if flag_parar.is_set():
-      break
-  #passar para proxima linha depois da mensagem.
-  print("\n")
-
-  #Salva mensagem da IA no historico também.
-  history.add_message_to_history(mensagem_final,"assistant")
-
-  return str(mensagem_final)
+        return str(mensagem_final)
