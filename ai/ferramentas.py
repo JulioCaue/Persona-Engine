bb491f699118f9f7c8f5c1ae2479c9075bae56ad
Julio <juliocaue22@gmail.com>
2026-09-13 16:12:55 -0300

    Agora contem tudo relevante as ferramentas.

--- ai/ferramentas.py
+++ ai/ferramentas.py
@@ -1,7 +1,16 @@
 import requests
 import serial
 import os
+from datetime import datetime
 bottango_url = "http://localhost:59224"
+
+MUSICAS = [
+    "daisy bell",
+    "hidden in the sand",
+    "hello world",
+    "sleeping in the cold below",
+    "twist"
+]
 
 def tocar_musica(nome: str):
     f"""
@@ -57,3 +66,69 @@ def parar_musica(nome: str):
     )
     response.raise_for_status()
     return f"Parando musica {nome}"
+
+def olhar_hora():
+    hora = datetime.now().strftime("%H:%M")
+    return f"Agora são {hora}"
+
+
+TOOLS = [
+    {
+        "type": "function",
+        "function": {
+            "name": "tocar_musica",
+            "description": "Toca uma música.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "nome": {
+                        "type": "string",
+                        "enum": MUSICAS
+                    }
+                },
+                "required": ["nome"]
+            }
+        }
+    },
+
+    {
+        "type": "function",
+        "function": {
+            "name": "parar_musica",
+            "description": "Para a música atual.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "nome": {
+                        "type": "string",
+                        "enum": MUSICAS
+                    }
+                },
+                "required": ["nome"]
+            }
+        }
+    },
+    
+    {
+        "type": "function",
+        "function": {
+            "name": "olhar_hora",
+            "description": (
+                "Obtém a hora atual do sistema."
+                "DEVE ser usada sempre que obter a hora atual for relevante na conversa. "
+                "Nunca reutilize horários encontrados no histórico, pois podem estar desatualizados."
+            ),
+            "parameters": {
+                "type": "object",
+                "properties": {}
+            }
+        }
+    }
+]
+
+
+FUNCOES = {
+    "tocar_musica": tocar_musica,
+    "parar_musica": parar_musica,
+    "olhar_hora": olhar_hora,
+}
