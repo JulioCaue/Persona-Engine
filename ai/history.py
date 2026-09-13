548d50985284142729b2ff6e3397703400218823
Julio <juliocaue22@gmail.com>
2026-09-13 16:13:36 -0300

    Adicionada função para colocar ferramentas no historico.

--- ai/history.py
+++ ai/history.py
@@ -7,7 +7,6 @@ e as retornar no formato esperado para o modelo.
 
 import json
 import os
-from datetime import datetime
 
 local_arquivo = "ai/prompts/conversa/historico.json"
 
@@ -20,13 +19,27 @@ def add_message_to_history(input,origem):
     with open(local_arquivo,"r",encoding="utf-8") as file:
         data = json.load(file)
 
-    data_atual = datetime.now()
-    data_atual = (
-        f"{data_atual.day}/{data_atual.month}/{data_atual.year}, {data_atual.hour}:{data_atual.minute}"
-    )
-
-    input = (f"Horario da mensagem: {data_atual}\n\nMensagem: {input}")
     input = {'role': origem, 'content': input}
+
+    data.append(input)
+    
+    with open(local_arquivo,"w",encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+def add_tool_usage_to_history(tool_calls):
+    """Adiciona uso da tool ao arquivo de historico."""
+    if not os.path.exists(local_arquivo):
+        with open(local_arquivo,"w",encoding="utf-8") as file:
+            json.dump([], file, ensure_ascii=False, indent=4)
+
+    with open(local_arquivo,"r",encoding="utf-8") as file:
+        data = json.load(file)
+
+
+    input = {
+        'role': "assistant", 
+        "tool_calls": tool_calls
+    }
 
     data.append(input)
     
