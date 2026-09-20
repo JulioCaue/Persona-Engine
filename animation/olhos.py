6c5e01323c57d9f54bfec9355efbc5731aec671d
Julio <juliocaue22@gmail.com>
2026-09-19 23:10:47 -0300

    Transformado as funções de rastreamento de rosto em classe.

--- animation/olhos.py
+++ animation/olhos.py
@@ -7,6 +7,7 @@ import mediapipe as mp
 import requests
 import cv2 as cv
 import time
+from logs import log_writer
 
 #suprimir mensagens de erros do ALSA
 from ctypes import cdll, CFUNCTYPE, c_char_p, c_int
@@ -21,106 +22,143 @@ except Exception:
 
 #continua normalmente
 
-def pegar_face_centro_atual(cap,detector):
-    """Olha o frame atual da webcam e retorna o centro de um rosto detectado."""
+class EyeTracker:
+    def __init__(self) -> None:
+        self.cap = cv.VideoCapture("/dev/video0") #caminho da camera
+        self.FaceDetector = mp.tasks.vision.FaceDetector
+        self.face_recognizer_model_path = 'testes/blaze_face_short_range.tflite'
+        self.BaseOptions = mp.tasks.BaseOptions
+        self.FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
+        self.VisionRunningMode = mp.tasks.vision.RunningMode
+        self.options = self.FaceDetectorOptions(
+            base_options = self.BaseOptions(model_asset_path = self.face_recognizer_model_path),
+            running_mode = self.VisionRunningMode.VIDEO
+        )
+        self.detector = self.FaceDetector.create_from_options(self.options)
+
+        # --- variaveis de movimento ---
+        self.min_x = 30
+        self.max_x = 170
+        self.olho_centro = 90
+        self.ALPHA = 0.75
+        self.angulo_anterior = None
+
+        #angulos estão na perspectiva da cabeça
+        self.angulo_max_direita = 30
+        self.angulo_max_esquerda = 170
+
+    def pegar_face_centro_atual(self):
+        """Olha o frame atual da webcam e retorna o centro de um rosto detectado."""
 
 
-    # Capturar frame por frame
-    ret, frame = cap.read()
-    
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        # Capturar frame por frame
+        ret, frame = self.cap.read()
 
-    timestamp_ms = time.monotonic_ns() // 1_000_000
-
-    detection_result = detector.detect_for_video(mp_image, timestamp_ms)
-
-    if detection_result.detections:
-        detections = detection_result.detections[0]
-        box = detections.bounding_box
-
-        image_x = box.origin_x
-        image_y = box.origin_y
-        width = box.width
-        height = box.height
-
-        #pega o maximo do eixo horizontal x (largura) e maximo vertical y (altura)
-        altura, largura = frame.shape[:2]
-
-        min_x = 0
-        max_x = largura - 1
-
-        min_y = 0
-        max_y = altura - 1
-
-        ponto_inicial = (image_x,image_y)
-        ponto_final = (image_x + width, image_y + height)
-
-        color = (0, 255, 0)
-
-        cv.rectangle(frame, ponto_inicial, ponto_final, color)
-
-        cv.imshow('Visão do modelo', frame)
-        
-        if cv.waitKey(1) == ord('q'):
+        if not ret:
             return
+        
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-        return image_x, image_y, max_x, min_x
+        timestamp_ms = time.monotonic_ns() // 1_000_000
+
+        detection_result = self.detector.detect_for_video(mp_image, timestamp_ms)
+
+        if detection_result.detections:
+            detections = detection_result.detections[0]
+            box = detections.bounding_box
+
+            image_x = box.origin_x
+            image_y = box.origin_y
+            width = box.width
+            height = box.height
+
+            #pega o maximo do eixo horizontal x (largura) e maximo vertical y (altura)
+            #altura atualmente não usado por conta dos olhos apenas seguindo eixo x
+            altura, largura = frame.shape[:2]
+
+            ponto_inicial = (image_x,image_y)
+            ponto_final = (image_x + width, image_y + height)
+
+            color = (0, 255, 0)
+
+            cv.rectangle(frame, ponto_inicial, ponto_final, color)
+
+            cv.imshow('Visão do modelo', frame)
+            
+            if cv.waitKey(1) == ord('q'):
+                return
+
+            centro_x = image_x + (width / 2)
+
+            #deixando aqui pois virá a ser util caso olhos ganhem movimento no eixo y
+            #centro_y = image_y + (height / 2)
+
+            return centro_x, largura
 
 
+    def seguir_rosto(self):
+        """
+        segue o rosto do usuario com os olhos (horizontal apenas).
+        """
 
-def seguir_rosto():
-    """
-    segue o rosto do usuario com os olhos (horizontal apenas).
-    """
+        with self.detector:
+            
+            if not self.cap.isOpened():
+                print("Camera não foi encontrada aberta")
+                exit()
+                return
+            # --- inicio da conexão ---
+            try:
+                while True:
+                    resultado_centro_rosto = self.pegar_face_centro_atual()
 
-    face_recognizer_model_path = 'testes/blaze_face_short_range.tflite'
+                    
 
-    BaseOptions = mp.tasks.BaseOptions
-    FaceDetector = mp.tasks.vision.FaceDetector
-    FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
-    VisionRunningMode = mp.tasks.vision.RunningMode
+                    if resultado_centro_rosto:
+                        centro_x, largura = resultado_centro_rosto
 
-    # Create a face detector instance with the image mode:
-    options = FaceDetectorOptions(
-        base_options=BaseOptions(model_asset_path = face_recognizer_model_path),
-        running_mode=VisionRunningMode.VIDEO
-    )
+                        angulo_atual = self.angulo_max_direita + (centro_x / largura) * (self.angulo_max_esquerda - self.angulo_max_direita)
 
-    # --- variaveis de movimento ---
-    min_x = 30
-    max_x = 170
-    olho_centro = 90
-    ALPHA = 0.8
-    angulo_anterior = olho_centro
+                        if not self.angulo_anterior:
+                            self.angulo_anterior = angulo_atual
+                            angulo_suavizado = angulo_atual
+
+                        else:
+                            #suaviza o angulo atual para movimentos menos brutos
+                            angulo_suavizado = (angulo_atual * self.ALPHA) + (self.angulo_anterior * (1 - self.ALPHA))
+
+                        #transforma angulo final em valor que bottango entende (entre 0.0 e 1.0)
+                        #conta: valor_bottango = (angulo - angulo_min) / (angulo_max - angulo_min)
+                        
+                        valor_angulo_final = round(
+                            (
+                                angulo_suavizado - self.angulo_max_direita
+                            ) / (
+                                self.angulo_max_esquerda - self.angulo_max_direita
+                            ), 3
+                        )
 
 
-    with FaceDetector.create_from_options(options) as detector:
-        cap = cv.VideoCapture("/dev/video0") #caminho da camera
-        if not cap.isOpened():
-            print("Camera não foi aberta")
-            exit()
-        # --- inicio da conexão ---
-        try:
-            while True:
-                resultado_frame = pegar_face_centro_atual(cap,detector)
+                        valor_angulo_final = max(0.0, min(1.0, valor_angulo_final))
 
-                if resultado_frame:
-                    image_x, width, max_x, min_x = resultado_frame
+                        #Envia angulos direto para o bottango pois o codigo do arduino que observa a porta serial é substituido quando uma animação do arduino é tocada.
 
-                    centro_x = image_x + width / 2
+                        if abs(valor_angulo_final - self.angulo_anterior) > 0.010:
+                            print(valor_angulo_final)
+                            response = requests.put(
+                                "http://localhost:59224/ControlInput/",
+                                json={
+                                    "identifier": "mexerOlhos",
+                                    "value": valor_angulo_final
+                                }
+                            )
 
-                #suaviza o angulo atual para movimentos menos brutos
-                angulo_suavizado = ALPHA * centro_x + (1 - ALPHA) * angulo_anterior
+                            response.raise_for_status()
+                        self.angulo_anterior = valor_angulo_final
 
-                #transforma angulo final em valor que bottango entende (entre 0.0 e 1.0)
-                valor_angulo_final = round((centro_x - min_x) / (max_x - min_x),3)
-                valor_angulo_final = max(0.0, min(1.0, valor_angulo_final))
-
-                #Envia angulos direto para o bottango pois o codigo do arduino que observa a porta serial é substituido quando uma animação do arduino é tocada.
-
-                if not angulo_anterior or abs(valor_angulo_final - angulo_anterior) > 0.010:
-                    print(valor_angulo_final)
-                    respose = requests.put(
+            except KeyboardInterrupt:
+                try:
+                    response = requests.put(
                         "http://localhost:59224/ControlInput/",
                         json={
                             "identifier": "mexerOlhos",
@@ -128,14 +166,15 @@ def seguir_rosto():
                         }
                     )
 
-                    respose.raise_for_status()
-                angulo_anterior = valor_angulo_final
+                    response.raise_for_status()
 
-        except Exception as e:
-            print(e)
+                except Exception as e:
+                    log_writer.write(__name__, f"Ocorreu um problema durante o fechamento do track de olhos: {e}")
 
-    cap.release()
-    cv.destroyAllWindows()
+            except Exception as e:
+                print(e)
+                log_writer.write(__name__, e)
 
 
-seguir_rosto()
+        self.cap.release()
+        cv.destroyAllWindows()
